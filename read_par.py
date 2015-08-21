Fix issue with RT calculation
Add debug statements for NHdr information

read_par.py
@@ -139,6 +139,13 @@ def read_par(parfilename):
             rows = []
             line = None
             first_row = None
+#1   1    1  1 0 2     0  16    50  256  256     0.00000   7.11331 8.67964e-03  1070  1860   0.00   0.00   0.00  -20.00   20.00   60.00 10.000 10.000 0 2 0 2  0.977  0.977   4.60    0.00     0.00    0.00   1   15.00     0    0    0    64   0.0  1   1    7    0   0.000    0.000    0.000  1
+            #col_types = (int, int, int, int, int, int, int, int, int, int, int,
+            #    float, float, float, int, int, float, float, float, float,
+            #    float, float, float, float, int, int, int, int, float,
+            #    float, float, float, float, float, int, float, int, int,
+            #    int, int, float, int, int, str, str, float, float, float,
+            #    int)
             while line != "":
                 line = parfile.readline()
                 if line.strip() == "":  # Empty line
@@ -148,9 +155,11 @@ def read_par(parfilename):
                 cols = line.strip().split()
                 #Grab first 11 cols, and 43rd for diffusion gradient number
                 row = [int(i) for i in cols[:11] + cols[42:43]]
+                #row = [col_types[i](x) for i, x in enumerate(cols, 0)]
                 if first_row is None:
                     first_row = cols
                 rows.append(row)
+            last_row = cols
             """
             #All columns
             rectypes = [('slice_number', int), ('echo_number', int),
@@ -195,11 +204,11 @@ def read_par(parfilename):
                 slice_index = np.concatenate((slice_index, zeros), axis=1)
             if par.dyn > 1:
                 #estimate scan-duration from dtime PAR file row
-                par.RT = (slice_index[-1,32] - slice_index[1,32]) / (par.dyn
+                par.RT = (float(last_row[32]) - float(first_row[32])) / (par.dyn
                     - 1)
             else:
                 par.RT = np.nan
-            par.slice_index = slice_index
+            par.slice_index = slice_index[:,0:12]
             #rectypes = [('slice_number', int), ('echo_number', int),
             #    ('dynamic_scan_number', int), ('cardiac_phase_number', int),
             #    ('image_type_mr', int), ('scanning_sequence', int),

write_nii.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import binascii
 import logging
 import numpy as np
 import struct
@@ -7,6 +8,7 @@ import struct
 def WriteNii(fname, NHdr, Data3D):
     logger = logging.getLogger('raw2nii')
     try:
+        logger.debug("Writing NHdr...")
         with open(fname, 'wb') as fid:
             #write header to nii binary
             fn = NHdr.fieldnames()
@@ -16,16 +18,31 @@ def WriteNii(fname, NHdr, Data3D):
                 val = hdrfield.val
                 if not isinstance(val, np.ndarray):  # If writing simple value
                     if prec == 's':  # Writing a string
-                        fid.write(val)
+                        packed = val
+                        logger.debug("Field {0}[{1}] (prec={2}, val='{3}', "
+                            "packed string='{4}')".format(key, fid.tell(), prec,
+                            val, packed))
+                        fid.write(packed)
                     else:
-                        fid.write(struct.pack(prec, val))
+                        packed = struct.pack(prec, val)
+                        logger.debug("Field {0}[{1}] (prec={2}, val='{3}', "
+                            "packed bytes='{4}')".format(key, fid.tell(), prec,
+                            val, binascii.hexlify(packed)))
+                        fid.write(packed)
                 else: # If writing array
                     if len(val.shape) == 1:  # If 1D array
-                        fid.write(struct.pack(prec * len(val),
-                            *list(val)))
+                        packed = struct.pack(prec * len(val), *list(val))
+                        logger.debug("Field {0}[{1}] (prec={2}, val='{3}', "
+                            "packed array='{4}')".format(key, fid.tell(), prec,
+                            val, binascii.hexlify(packed)))
+                        fid.write(packed)
                     else:
                         #Transpose matrix before writing to get Fortran order
-                        val.astype(prec).T.tofile(fid)
+                        packed = val.astype(prec).T
+                        logger.debug("Field {0}[{1}] (prec={2}, val='{3}', "
+                            "packed matrix='{4}')".format(key, fid.tell(), prec,
+                            val, binascii.hexlify(packed.tobytes())))
+                        packed.tofile(fid)
                 fid.flush()
             if 8 == NHdr.bitpix.val:
                 bitpixstr = 'int8'