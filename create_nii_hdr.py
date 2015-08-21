Fix issue with RT calculation
Add debug statements for NHdr information

create_nii_hdr.py
@@ -8,6 +8,32 @@ from calc_angulation import calc_angulation
 from nifti_mat44_to_quatern import nifti_mat44_to_quatern
 
 
+FILLER_CHAR = '\0'  # Used to pad strings
+
+#           Struct precision values reference
+#                                          Standard size
+# format  C type              Python type  (in bytes)
+#---------------------------------------------------------
+# x       pad byte	          no value
+# c	      char	              string of    1
+#                               length 1
+# b	      signed char	      integer	   1
+# B       unsigned char	      integer	   1
+# ?	      _Bool	              bool	       1
+# h       short	              integer	   2
+# H       unsigned short      integer	   2
+# i       int	              integer	   4
+# I       unsigned int	      integer	   4
+# l       long	              integer	   4
+# L	      unsigned long	      integer	   4
+# q       long long	          integer	   8
+# Q       unsigned long long  integer	   8
+# f       float	              float	       4
+# d       double	          float	       8
+# s       char[]	          string
+# p       char[]	          string
+# P       void *	          integer
+
 class NiiHdr:
     def fieldnames(self):
         return ('HdrSz', 'Data_Type', 'db_name', 'extents', 'session_error',
@@ -38,28 +64,32 @@ def CreateNiiHdr(par, angulation=None, rescale=None, dim=None):
     """
         create Nifti header from parameters as read from PAR file
     """
+    logger = logging.getLogger('raw2nii')
     M, realvoxsize = calc_angulation(par, angulation)
     qoffset_xyz, quatern_bcd, qfac = nifti_mat44_to_quatern(M)
     NHdr = NiiHdr()
     NHdr.HdrSz = NiiHdrField(348, 'i')
-    NHdr.Data_Type = NiiHdrField(chr(32) * 10, 's')
-    NHdr.db_name = NiiHdrField(chr(32) * 18, 's')
+    NHdr.Data_Type = NiiHdrField(FILLER_CHAR * 10, 's')
+    NHdr.db_name = NiiHdrField(FILLER_CHAR * 18, 's')
     NHdr.extents = NiiHdrField(0, 'i')
     NHdr.session_error = NiiHdrField(0, 'h')
     NHdr.regular = NiiHdrField(114, 'B')
     NHdr.dim_info = NiiHdrField(0, 'B')
+    # Note on pixdim: use real voxel dimensions as calculated from
+    # FOV/matrixsize in approp direction (CHECK!). Because for older Philips
+    # releases, voxel dimensions in PAR file slice lines are rounded to 0.1!
     if 3 == dim[0]:
         NHdr.dim = NiiHdrField(np.concatenate(([dim[0]], par.dim,
             [1, 1, 1, 1])), 'h')
         if np.nan is par.RT:
             par.RT = 1
-        pixdim = NiiHdrField(np.concatenate(([qfac], realvoxsize,
-            [par.RT, 1, 1, 1])), 'f')
+        NHdr.pixdim = NiiHdrField(np.concatenate(([qfac], realvoxsize,
+            [par.RT, 0, 0, 0])), 'f')
     elif 4 == dim[0]:
         NHdr.dim = NiiHdrField(np.concatenate(([dim[0]], par.dim,
             [dim[1], 1, 1, 1])), 'h')
-        pixdim = NiiHdrField(np.concatenate(([qfac], realvoxsize,
-            [par.RT, 1, 1, 1])), 'f')
+        NHdr.pixdim = NiiHdrField(np.concatenate(([qfac], realvoxsize,
+            [par.RT, 0, 0, 0])), 'f')
     elif 5 == dim[0]:
         #Find real dimension
         if dim[2] == 1:
@@ -70,8 +100,8 @@ def CreateNiiHdr(par, angulation=None, rescale=None, dim=None):
             realdim = np.array([dim[2], 1, 1])
         NHdr.dim = NiiHdrField(np.concatenate(([filedim], par.dim, [dim[1]],
             realdim)), 'h')
-        pixdim = NiiHdrField(np.concatenate(([qfac], realvoxsize,
-            [par.RT, 1, 1, 1])), 'f')
+        NHdr.pixdim = NiiHdrField(np.concatenate(([qfac], realvoxsize,
+            [par.RT, 0, 0, 0])), 'f')
     NHdr.intent_p123 = NiiHdrField(np.array([0, 0, 0]), 'f')
     NHdr.intent_code = NiiHdrField(0, 'h')
     if 8 == par.bit:
@@ -85,10 +115,6 @@ def CreateNiiHdr(par, angulation=None, rescale=None, dim=None):
     NHdr.datatype = NiiHdrField(dt, 'h')
     NHdr.bitpix = NiiHdrField(par.bit, 'h')
     NHdr.slice_start = NiiHdrField(0, 'h')
-    # use real voxel dimensions as calculated from FOV/matrixsize in approp
-    # direction (CHECK!). Because for older Philips releases, voxel dimensions
-    # in PAR file slice lines are rounded to 0.1!
-    NHdr.pixdim = pixdim
     NHdr.vox_offset = NiiHdrField(352, 'f')
     if rescale == 1:
         rs = par.rescale_slope
@@ -109,13 +135,13 @@ def CreateNiiHdr(par, angulation=None, rescale=None, dim=None):
     descrip = "{0}; converted by raw2nii {1}".format(par.name,
         raw2nii_version.VERSION)[:80].ljust(80)
     NHdr.descrip = NiiHdrField(descrip, 's')
-    NHdr.aux_file = NiiHdrField(chr(32) * 24, 's')
+    NHdr.aux_file = NiiHdrField(FILLER_CHAR * 24, 's')
     NHdr.qform_code = NiiHdrField(nifti_defines.kNIFTI_XFORM_SCANNER_ANAT, 'h')
     NHdr.sform_code = NiiHdrField(nifti_defines.kNIFTI_XFORM_SCANNER_ANAT, 'h')
     NHdr.quatern_bcd = NiiHdrField(quatern_bcd, 'f')
     NHdr.qoffset_xyz = NiiHdrField(qoffset_xyz, 'f')
     NHdr.srow_xyz = NiiHdrField(M[0:3,:].T, 'f')
-    NHdr.intent_name = NiiHdrField(chr(32) * 16, 's')
+    NHdr.intent_name = NiiHdrField(FILLER_CHAR * 16, 's')
     NHdr.magic = NiiHdrField(nifti_defines.kNIFTI_MAGIC_EMBEDDED_HDR, 'i')
     return NHdr
 