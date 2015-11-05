3f5b17bf5747a8214421a3481dc16c50a95e01d4
Matthew Christian <matthew.d.christian@vanderbilt.edu>
2015-11-05 10:53:04 -0600

    Add unit test case to test_data.py
    Changed variable names 'test*' and 'output* to 'canon*' and 'test*'
    respectively to better represent the datasets. test_data.py now runs a default
    set of tests when no arguments are specified on the command line.

--- test_data.py
+++ test_data.py
@@ -11,16 +11,19 @@ from project import convert_raw2nii
 from tools.nii_info import read_nii
 
 
-def main(test_data_folder, test_folder, rtol, atol):
-    """
-        Runs convert_raw2nii on a folder containing PARREC test data and
-        compares the NIFTI output
-    """
+DEFAULT_TEST_DATA = [
+    ('canonicalData', 'philips/philips_3T_achieva/fMRI/')
+]
+
+
+def run_test(canon_data_folder, trial_folder, rtol, atol):
+    """ Runs convert_raw2nii on a folder containing PARREC test data and
+        compares the NIFTI output """
     logger = logging.getLogger('raw2nii_test')
-    test_data_folder = os.path.abspath(test_data_folder)
-    path_par = os.path.join(test_data_folder, test_folder, "PARREC")
-    input_par = glob.glob("{0}/*.PAR".format(path_par))[0]
-    out_folder = os.path.join("testOutput", test_folder, "NIFTI")
+    canon_data_folder = os.path.abspath(canon_data_folder)
+    path_par = os.path.join(canon_data_folder, trial_folder, 'PARREC')
+    input_par = glob.glob('{0}/*.PAR'.format(path_par))[0]
+    test_folder = os.path.join('testOutput', trial_folder, 'NIFTI')
     #Set up convert_raw2nii logger
     raw2nii_logger = logging.getLogger('raw2nii')
     #raw2nii_logger.setLevel(logging.DEBUG)
@@ -29,84 +32,77 @@ def main(test_data_folder, test_folder, rtol, atol):
     _stream_handler.setFormatter(_formatter)
     raw2nii_logger.addHandler(_stream_handler)
     #Run NIFTI conversion function and output to testOutput directory
-    output = convert_raw2nii(filelist=[input_par], prefix="", suffix="",
-        pathpar=path_par, outfolder=out_folder, outputformat=1, angulation=1,
+    logger.info('Running test: parfile={0} path_par={1} test_folder={2}'.format(
+        input_par, path_par, test_folder))
+    output = convert_raw2nii(filelist=[input_par], prefix='', suffix='',
+        pathpar=path_par, outfolder=test_folder, outputformat=1, angulation=1,
         rescale=1, dti_revertb0=0)
     #Compare to the 4D data since we have the Vanderbilt override behavior
-    test_nifti_folder = os.path.join(test_data_folder, test_folder, "NIFTI",
-        "4D")
+    canon_nifti_folder = os.path.join(canon_data_folder, trial_folder, 'NIFTI',
+        '4D')
     #Produce listing of each file in NIFTI folders in lexographical order and
     #prepare each pair for comparison
-    test_niftis = sorted(glob.glob("{0}/*.nii".format(test_nifti_folder)))
-    output_niftis = sorted(glob.glob("{0}/*.nii".format(out_folder)))
-    if len(test_niftis) != len(output_niftis):
+    canon_niftis = sorted(glob.glob('{0}/*.nii'.format(canon_nifti_folder)))
+    test_niftis = sorted(glob.glob('{0}/*.nii'.format(test_folder)))
+    if len(canon_niftis) != len(test_niftis):
         logger.error('Different number of output files! Test niftis: {0} '
-            'Output niftis: {1}'.format(pprint.pformat(test_niftis),
-            pprint.pformat(output_niftis)))
-    output_pairs = zip(test_niftis, output_niftis)
-    for test_nifti, output_nifti in output_pairs:
-        logger.info("Output pair: {0} -> {1}".format(test_nifti, output_nifti))
+            'Output niftis: {1}'.format(pprint.pformat(canon_niftis),
+            pprint.pformat(test_niftis)))
+    file_pairs = zip(canon_niftis, test_niftis)
+    for canon_nifti, test_nifti in file_pairs:
+        logger.info('File pair: {0} -> {1}'.format(canon_nifti, test_nifti))
+        canon_header, canon_body = read_nii(canon_nifti)
         test_header, test_body = read_nii(test_nifti)
-        output_header, output_body = read_nii(output_nifti)
         #First compare the headers of the two niftis
-        for test_key, test_val in test_header.items():
-            output_val = output_header[test_key]
-            #logger.debug("Compare header.{0} : {1} -> {2}".format(
-            #    test_key, test_val, output_val))
-            if isinstance(test_val, np.ndarray):
-                is_different = not np.allclose(test_val, output_val, rtol, atol)
-            elif isinstance(test_val, float):
-                is_different = abs(test_val - output_val) > (atol + rtol *
-                    abs(output_val))
+        for canon_key, canon_val in canon_header.items():
+            test_val = test_header[canon_key]
+            #logger.debug('Compare header.{0} : {1} -> {2}'.format(canon_key,
+            #   canon_val, test_val))
+            if isinstance(canon_val, np.ndarray):
+                is_different = not np.allclose(canon_val, test_val, rtol, atol)
+            elif isinstance(canon_val, float):
+                is_different = abs(canon_val - test_val) > (atol + rtol *
+                    abs(test_val))
             else:
-                is_different = (test_val != output_val)
+                is_different = (canon_val != test_val)
             if is_different:
-                logger.warning("header.{0} are different: {1} -> {2}".format(
-                    test_key, test_val, output_val))
+                logger.warning('header.{0} are different: {1} -> {2}'.format(
+                    canon_key, canon_val, test_val))
         #Then compare the bodies of the niftis
-        if test_body['dtype'] != output_body['dtype']:
-            logger.warning("Body dtypes are different: {0} -> {1}".format(
-                test_body['dtype'], output_body['dtype']))
-        elif test_body['data'].shape != output_body['data'].shape:
-            logger.warning("Body shapes are different: {0} -> {1}".format(
-                test_body['dtype'].shape, output_body['dtype'].shape))
-        elif not np.allclose(test_body['data'], output_body['data'], rtol,
-                atol):
-            diff_output, err_output = body_diff_script(logger, test_body,
-                output_body)
-            logger.warning("Body data is different: <{0}> <{1}>".format(
+        if canon_body['dtype'] != test_body['dtype']:
+            logger.warning('Body dtypes are different: {0} -> {1}'.format(
+                canon_body['dtype'], test_body['dtype']))
+        elif canon_body['data'].shape != test_body['data'].shape:
+            logger.warning('Body shapes are different: {0} -> {1}'.format(
+                canon_body['dtype'].shape, test_body['dtype'].shape))
+        elif not np.allclose(canon_body['data'], test_body['data'], rtol, atol):
+            diff_output, err_output = subprocess.Popen([
+                './tools/nii_body_diff.sh', canon_nifti, test_nifti],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            logger.warning('Body data is different:\n{0}{1}'.format(
                 diff_output, err_output))
     return 0
 
-def body_diff_script(logger, test_body, output_body):
-    #Write out test binary data to a tempfile
-    test_bin_fd, test_bin_name = tempfile.mkstemp()
-    with os.fdopen(test_bin_fd, 'wb') as f:
-        test_body['data'].astype(test_body['dtype']).T.tofile(f)
-    #Write out canonical binary data to a tempfile
-    output_bin_fd, output_bin_name = tempfile.mkstemp()
-    with os.fdopen(output_bin_fd, 'wb') as f:
-        output_body['data'].astype(output_body['dtype']).T.tofile(f)
-    #Compare two tempfiles
-    return subprocess.Popen(['./tools/nii_body_diff.sh', test_bin_name,
-        output_bin_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        ).communicate()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     logger = logging.getLogger('raw2nii_test')
     logger.setLevel(logging.INFO)
-    _formatter = logging.Formatter("%(levelname)s: %(message)s")
+    _formatter = logging.Formatter('%(levelname)s: %(message)s')
     _stream_handler = logging.StreamHandler()
     _stream_handler.setFormatter(_formatter)
     logger.addHandler(_stream_handler)
     parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", "-d", action="store_true")
-    parser.add_argument("test_data_folder")
-    parser.add_argument("test_folder")
-    parser.add_argument("--rel-tolerance", "-r", type=float, default=1e-05)
-    parser.add_argument("--abs-tolerance", "-a", type=float, default=1e-08)
+    parser.add_argument('--debug', '-d', action='store_true')
+    parser.add_argument('canon_data_folder', nargs='?')
+    parser.add_argument('trial_folder', nargs='?')
+    parser.add_argument('--rel-tolerance', '-r', type=float, default=1e-05)
+    parser.add_argument('--abs-tolerance', '-a', type=float, default=1e-08)
     options = parser.parse_args()
+    if options.canon_data_folder and options.trial_folder:
+        test_args = [(options.canon_data_folder, options.trial_folder)]
+    else:
+        test_args = DEFAULT_TEST_DATA
     if options.debug:
         logger.setLevel(logging.DEBUG)
-    main(options.test_data_folder, options.test_folder, options.rel_tolerance,
-        options.abs_tolerance)
+    for canon_data_folder, trial_folder in test_args:
+        run_test(canon_data_folder, trial_folder, options.rel_tolerance,
+            options.abs_tolerance)
