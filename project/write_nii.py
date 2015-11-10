a062427c07c338545eb2b41d2ce812fa1e73fa4f
Matthew Christian <matthew.d.christian@vanderbilt.edu>
2015-11-10 10:06:35 -0600

    Fix issue with floating point matrix type
    When multiple scaling factors are present, the data should be translated to
    32-bit floating point. Bug was that 64-bit floating point was being used (type
    of 'float' instead of 'float32').

--- project/write_nii.py
+++ project/write_nii.py
@@ -24,7 +24,7 @@ def WriteNii(fname, NHdr, Data3D, issue):
             NHdr.write_to_file(fid)  # write header to nii binary
             if NHdr.bitpix.val in (8, 16, 32, 64):
                 if NHdr.bitpix.val == 32 and issue:
-                    bitpixstr = 'float'
+                    bitpixstr = 'float32'
                 else:
                     #Using a form of ternary statements here
                     u_prefix = {True: 'u', False: ''}[
@@ -33,12 +33,10 @@ def WriteNii(fname, NHdr, Data3D, issue):
             #now add 4 extra bytes in space between header and offset for data
             #indicating that single .nii file ("n+1\0") rather than separate
             #img/hdr files were written. see http://nifti.nimh.nih.gov
-            niftitag = bytearray(binascii.unhexlify('6e2b3100'))
-            fid.write(niftitag)
+            fid.write(bytearray(binascii.unhexlify('6e2b3100')))
             #add remaining 0s (probably not required)
-            padding = bytearray([0] * (NHdr.vox_offset.val - NHdr.HdrSz.val - 4))
-            if padding:
-                fid.write(padding)
+            fid.write(bytearray([0] * (NHdr.vox_offset.val - NHdr.HdrSz.val
+                - 4)))
             for s3 in range(Data3D.shape[2]):
                 #flip data in order to write out
                 Data3D[:,:,s3] = np.fliplr(Data3D[:,:,s3])
